282a21945cb1a4e7855d845722c4b76eb97246ef
JakubDuma <Dzimi@DESKTOP-FRHBECJ>
2025-01-03 23:19:29 +0100

    Add project files.

---
+++ Course_ASP_Week3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Course_ASP_Week3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
